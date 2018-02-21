7b22e52f522586ccb4cd5f34fc19e937f45ef847
anbudhanush5 <35687834+anbudhanush5@users.noreply.github.com>
2018-02-21 14:08:37 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[10],i,sum=0;
+    int avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }
+   
+    avg=sum/n;
+    
+    printf("%d",;
+    getch();
+}
