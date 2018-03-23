4a9ef4cde29ecaf8c84140db13d02000114207bf
anbu <35687834+anbudhanush5@users.noreply.github.com>
2018-03-23 16:20:33 +0530

    Create 007.c

---
+++ 007.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,count=0,j,k;
+char a[100],t;
+scanf("%[^\n]s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+count++;
+}
+for(i=0;i<count;i++)
+{
+if(i%2!=0)
+{
+t=a[i];
+a[i]=a[i-1];
+a[i-1]=t;
+}
+}
+printf("%s",a);
+getch();
+}
